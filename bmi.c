697340273cb9d86fc8eca427a25cc098f3553a57
AntiOpsecRaccoon <zabalajaziel@proton.me>
2026-07-10 09:01:00 +0000

    Add files via upload

--- bmi.c
+++ bmi.c
@@ -6,15 +6,42 @@ float IMC;
 
 int main(void)
 {
-	printf ("inserte peso ");
-	scanf("%d", &peso);
-	printf("inserte altura en cm ");
-	scanf("%d", &altura);
-	IMC = (float)peso/((altura*altura)/100);
-    printf("tu IMC es: %f " , IMC*100);
-	printf("valores iMC \t\n");
-	printf("bajo peso: menor 18.5 \t\n");
-	printf("normal:18.5 - 24.9 \t\n");
-	printf("sobrepeso: 25 - 30 \t\n");
+
+	do
+
+	{
+		printf("Inserte peso (no negativo): ");
+		scanf("%d", &peso);
+		if (peso < 0)
+		{
+			printf("Error: el peso no puede ser negativo. Intente de nuevo.\n");
+		}
+	}
+
+	while (peso < 0);
+
+	do
+
+	{
+		printf("Inserte altura en cm (no negativa): ");
+		scanf("%d", &altura);
+		if (altura < 0)
+
+		{
+			printf("Error: la altura no puede ser negativa. Intente de nuevo.\n");
+		}
+
+	}
+
+	while (altura < 0);
+
+	IMC = (float)peso / ((altura * altura) / 10000.0);
+	printf("Tu IMC es: %.2f\n", IMC);
+
+	printf("Valores IMC:\n");
+	printf("Bajo peso: menor a 18.5\n");
+	printf("Normal: 18.5 - 24.9\n");
+	printf("Sobrepeso: 25 - 30\n");
+
 	return 0;
 }

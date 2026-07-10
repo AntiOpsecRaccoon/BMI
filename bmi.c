2a846cc5db90cc07b03bb760920344d4f89b3ca8
AntiOpsecRaccoon <zabalajaziel@proton.me>
2026-07-10 08:40:14 +0000

    Add files via upload

---
+++ bmi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int peso;
+int altura;
+float IMC;
+
+int main(void)
+{
+	printf ("inserte peso ");
+	scanf("%d", &peso);
+	printf("inserte altura en cm ");
+	scanf("%d", &altura);
+	IMC = (float)peso/((altura*altura)/100);
+    printf("tu IMC es: %f " , IMC*100);
+	printf("valores iMC \t\n");
+	printf("bajo peso: menor 18.5 \t\n");
+	printf("normal:18.5 - 24.9 \t\n");
+	printf("sobrepeso: 25 - 30 \t\n");
+	return 0;
+}
